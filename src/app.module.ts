4598d9424b86dd0bef0da6910b289193b9d62991
Rodrigo Klaes <rodrigoklaes@gmail.com>
2023-02-16 21:50:40 -0300

    feat: created initial structur to user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [CompaniesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
