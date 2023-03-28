b0a7cd5b256bf5b180948006edbe0eb9e5b7ef9a
Rodrigo Klaes <rodrigoklaes@gmail.com>
2023-03-27 22:18:59 -0300

    feat: created auth design structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CompaniesModule, UsersModule],
+  imports: [CompaniesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
